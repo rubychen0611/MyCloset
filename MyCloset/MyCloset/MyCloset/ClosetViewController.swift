d1fd3540fd74260958e633f1147107e8dfd33696
rubychen0611 <rubychen0611@yeah.net>
2017-11-16 04:54:03 -0800

    first tableview

--- MyCloset/MyCloset/MyCloset/ClosetViewController.swift
+++ MyCloset/MyCloset/MyCloset/ClosetViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
-class ClosetViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ClosetViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    private let clothestypes = ["上衣","下装","外套","鞋子","包","配饰"]
+    let leftTableIdentifier = "leftTableleftTableIdentifier"
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
@@ -31,5 +35,25 @@ class ClosetViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    //MARK:Table View Data Sourse Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return clothestypes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        tableView.rowHeight = 70
+        var cell = tableView.dequeueReusableCell(withIdentifier: leftTableIdentifier)
+        if(cell == nil)
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier:leftTableIdentifier)
+        }
+        cell?.textLabel?.text = clothestypes[indexPath.row]
+        cell?.textLabel?.font = UIFont.boldSystemFont(ofSize:20)
+        cell?.backgroundColor = UIColor.clear
+        cell?.textLabel?.textAlignment = NSTextAlignment.center
+        return cell!
+    }
 }
