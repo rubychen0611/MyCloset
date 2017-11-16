8aebfac9ff7204e25b672dd17cc449275a9d6c7a
rubychen0611 <rubychen0611@yeah.net>
2017-11-16 00:21:29 -0800

    created Tab bar

---
+++ MyCloset/MyCloset/MyCloset/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCloset
+//
+//  Created by 陈紫琦 on 2017/11/16.
+//  Copyright © 2017年 nju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
