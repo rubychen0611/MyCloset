f1aace09af9868ed27a3c09da31220dfb8146d62
rubychen0611 <rubychen0611@yeah.net>
2017-11-17 22:40:03 -0800

    added collection view

--- MyCloset/MyCloset/MyCloset/ClosetViewController.swift
+++ MyCloset/MyCloset/MyCloset/ClosetViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ClosetViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+class ClosetViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout
 {
-    private let clothestypes = ["上衣","下装","外套","鞋子","包","配饰"]
+    private let clothestypes = ["上衣","下装","外套","连衣裙","鞋子","包","配饰"]
+    private let uppertypes = ["短袖", "T袖", "衬衫", "卫衣", "马甲", "其他上衣"]
     let leftTableIdentifier = "leftTableleftTableIdentifier"
+    let rightTableIdentifier = "rightTableleftTableIdentifier"
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -36,7 +38,7 @@ class ClosetViewController: UIViewController, UITableViewDataSource, UITableView
     }
     */
     
-    //MARK:Table View Data Sourse Methods
+    //MARK:Table View 代理方法
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return clothestypes.count
@@ -44,16 +46,41 @@ class ClosetViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        tableView.rowHeight = 70
+        tableView.rowHeight = 70    //cell高度
         var cell = tableView.dequeueReusableCell(withIdentifier: leftTableIdentifier)
         if(cell == nil)
         {
             cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier:leftTableIdentifier)
         }
         cell?.textLabel?.text = clothestypes[indexPath.row]
-        cell?.textLabel?.font = UIFont.boldSystemFont(ofSize:20)
-        cell?.backgroundColor = UIColor.clear
-        cell?.textLabel?.textAlignment = NSTextAlignment.center
+        cell?.textLabel?.font = UIFont.boldSystemFont(ofSize:18) //字体、大小
+        cell?.backgroundColor = UIColor.clear //背景色透明
+        cell?.textLabel?.textAlignment = NSTextAlignment.center //剧中显示
         return cell!
+    }
+    
+    //MARK: Collection View 代理方法
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+    {
+        return 1 //Section个数
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return uppertypes.count //Section中Item的个数
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        //var cell = collectionView.dequeueReusableCell(withIdentifier: rightTableIdentifier)
+        //if(cell == nil)
+        //{
+            //cell = UICollectionViewCell(style:UICollectionViewCellStyle.default, reuseIdentifier:rightTableIdentifier)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ClosetCollectionViewCell", for: indexPath) as! ContentCell
+       // }
+        cell.imageView.image = UIImage(named: "defaultPhoto.png")
+        cell.label.text = uppertypes[indexPath.row]
+        
+        return cell
     }
 }
---
+++ MyCloset/MyCloset/MyCloset/ContentCell.swift
@@ -0,0 +1,20 @@
+//
+//  ContentCell.swift
+//  MyCloset
+//
+//  Created by 陈紫琦 on 2017/11/16.
+//  Copyright © 2017年 nju. All rights reserved.
+//
+
+import UIKit
+
+class ContentCell: UICollectionViewCell
+{
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
